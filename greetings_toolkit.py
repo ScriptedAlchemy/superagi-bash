1aea1306158551f30e63d64fe11119b3094fe57b
Zack Jackson <zackary.l.jackson@gmail.com>
2023-09-10 04:22:56 -0700

    Update greetings_toolkit.py

--- greetings_toolkit.py
+++ greetings_toolkit.py
@@ -1,15 +1,18 @@
 from abc import ABC
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import Type, List
+import subprocess
+import shlex
+from pydantic import BaseModel, Field
 from greetings_tool import GreetingsTool
 
-
-class GreetingsToolkit(BaseToolkit, ABC):
-    name: str = "Greetings Toolkit"
-    description: str = "Greetings Tool kit contains all tools related to Greetings"
+# Update the Toolkit class
+class BashCommandToolkit(BaseToolkit, ABC):
+    name: str = "Bash Command Toolkit"
+    description: str = "Toolkit contains all tools related to Bash Command execution"
 
     def get_tools(self) -> List[BaseTool]:
-        return [GreetingsTool()]
+        return [BashCommandTool()]
 
-    def get_env_keys(self) -> List[str]:
-        return ["FROM"]
+    # def get_env_keys(self) -> List[str]:
+    #     return []
