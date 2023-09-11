5160e4b890cf7797b8fca9d4a583903e4fe92048
Zack Jackson <zackary.l.jackson@gmail.com>
2023-09-11 04:14:27 -0700

    e

--- bash_tool.py
+++ bash_tool.py
@@ -45,7 +45,8 @@ class BashCommandTool(BaseTool):
             result = subprocess.run(command,cwd=output_directory, shell=True, check=True, stdout=subprocess.PIPE)
             return f"{result.stdout.decode('utf-8')}"
         except subprocess.CalledProcessError as e:
-            return f"Command '{command}' returned non-zero exit status: {e}."
+            return f"Command '{command}' returned non-zero exit status: {e.returncode}. Error: {e.stderr.decode('utf-8')}"
+
 
 
 
