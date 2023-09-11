c90f496e568d3aedefc6e0b2f1b256d9560f33f7
Zack Jackson <zackary.l.jackson@gmail.com>
2023-09-10 17:00:15 -0700

    fix

--- bash_tool.py
+++ bash_tool.py
@@ -1,10 +1,21 @@
-import os
 from abc import ABC
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import Type, List
 import subprocess
 import shlex
 from pydantic import BaseModel, Field
+import os
+
+class BashCommandInput(BaseModel):
+    command: str = Field(..., description="Bash command to be executed")
+
+# Function to validate bash command
+def validate_command(command: str) -> bool:
+    try:
+        shlex.split(command)
+        return True
+    except ValueError:
+        return False
 
 class BashCommandTool(BaseTool):
     """
@@ -12,20 +23,12 @@ class BashCommandTool(BaseTool):
     """
     name: str = "Bash Command Tool"
     args_schema: Type[BaseModel] = BashCommandInput
-    description: str = "Executes a Bash Command"
+    description: str = "Executes a Bash Command"  # Fixed indentation here
 
     def _execute(self, command: str = None):
-        # Check current working directory
-        current_dir = os.getcwd()
-        target_dir = "/app/workspace"
-
-        # Change directory if not in target directory
-        if current_dir != "/app":
-            os.chdir(target_dir)
-
         if not validate_command(command):
             return "Invalid bash command."
-
+        
         try:
             result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE)
             return result.stdout.decode('utf-8')
